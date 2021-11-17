b8a2b65a0233d99d3fc2c428f83fd1f27ad31ad3
pyacat <77725817+pyacat@users.noreply.github.com>
2021-11-17 21:48:22 +0800

    change to php

---
+++ front.php
@@ -0,0 +1,67 @@
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+	<title>Log-In Form</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="final1form.css">
+	<script type="text/javaScript" src=""></script>
+</head>
+
+<body>
+
+
+	<div class="signup-form ">
+
+		<form name="RegForm" action="project4.html" onsubmit="" method="post">
+
+			<div style="float: right;margin-top: 6cm;">
+				<div class="text-center head">We are excited to see you!</div>
+
+				<div class="form-group margintop1">
+					<div class="input-group ">
+						<div class="input-group-prepend">
+							<span class="input-group-text">
+								<span class="fa fa-user"></span>
+							</span>
+						</div>
+						<input type="number" class="form-control" name="studentnumber" placeholder="Student Number">
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text">
+								<i class="fa fa-lock"></i>
+							</span>
+						</div>
+						<input type="password" class="form-control" name="password" placeholder="Password">
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="form-check-label"><input type="checkbox" required="required"> I accept the <a href="">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
+				</div>
+				<div class="form-group text-center ">
+					<button type="submit" class="btn btn-primary ">Sign In</button>
+				</div>
+				<div class="text-center"><a href="">Forgot Password?</a></div>
+		</form>
+
+		<div class="form-group text-center margintop1">
+			<hr style="margin-left: .1cm; width: 100%; height:2px;border-width:0;color:gray;background-color:gray">
+
+			<button type="submit" class="btn btn-primary ">Create New Account</button>
+		</div>
+
+	</div>
+
+	</div>
+</body>
+
+</html>
